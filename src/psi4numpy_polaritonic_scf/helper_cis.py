b849dfa224177ed998962487e36b553a39ab2170
FoleyLab <foleyj10@wpunj.edu>
2021-07-27 14:36:42 -0400

    added model Hamiltonian to Hilbert
    In order for model Hamiltonian (Pauli-Fierz) to match our CQED-CIS, we need to have access to the ground-state and first-excited state total dipole moments, as well as the transition dipole moments!  Working on making a helper_cis.py that will compute the total dipole moments for the ground- and excited-states, since the latter are not available in psi4 to my knowledge!

---
+++ src/psi4numpy_polaritonic_scf/helper_cis.py
@@ -0,0 +1,13 @@
+"""
+Helper function for CQED_RHF
+
+"""
+
+__authors__   = ["Jon McTague", "Jonathan Foley"]
+__credits__   = ["Jon McTague", "Jonathan Foley"]
+
+__copyright_amp__ = "(c) 2014-2018, The Psi4NumPy Developers"
+__license__   = "BSD-3-Clause"
+__date__      = "2021-01-15"
+
+
