95932c49c79572f8885a383787e433c602d854fd
MarcLie <mliebenthal@fsu.edu>
2024-11-19 16:13:08 -0500

    moved rdm codes to eom_build group. These files are big and should be treated differently.

--- cmake/cc_cavity.cmake
+++ cmake/cc_cavity.cmake
@@ -86,10 +86,8 @@ set(qed_eom_cc
 
         src/cc_cavity/src/ccsd/eom_ee_ccsd.cc
         src/cc_cavity/src/ccsd/eom_ea_ccsd.cc
-        src/cc_cavity/src/ccsd/eom_ee_rdm.cc
 
         src/cc_cavity/src/qed_ccsd_21/eom_ee_qed_ccsd_21.cc
-        src/cc_cavity/src/qed_ccsd_21/eom_ee_qed_rdm_21.cc
         src/cc_cavity/src/qed_ccsd_21/eom_ea_qed_ccsd_21.cc
 
 )
@@ -108,6 +106,7 @@ set(qed_eom_builds
         src/cc_cavity/src/ccsd/sigma_builds/eom_ea/sigma_00_4.cc
         src/cc_cavity/src/ccsd/sigma_builds/eom_ea/intermediates_00.cc
 
+        src/cc_cavity/src/ccsd/eom_ee_rdm.cc
 )
 set(qed_eom_builds
         ${qed_eom_builds} # QED-CCSD-21
@@ -126,6 +125,8 @@ set(qed_eom_builds
         src/cc_cavity/src/qed_ccsd_21/sigma_builds/eom_ea/sigma_21_3.cc
         src/cc_cavity/src/qed_ccsd_21/sigma_builds/eom_ea/sigma_21_4.cc
         src/cc_cavity/src/qed_ccsd_21/sigma_builds/eom_ea/intermediates_21.cc
+
+        src/cc_cavity/src/qed_ccsd_21/eom_ee_qed_rdm_21.cc
 )
 
 # the following files take a long time to compile with -fvar-tracking-assignments; disable it for these files
